3edeea2bdb28dfa63c95ac7a1d764e0d69cb0ae1
Billy Jones <billyjones26@gmail.com>
2014-12-06 12:42:15 +0000

    Just commiting progress

--- orchestrate.js
+++ orchestrate.js
@@ -1,46 +1,231 @@
+var _ = require('lodash');
+
 (function () {
 	
-	var Orchestrate = function(options) {
-		this.options = options;
+    var error = null,
+        queries = [],
+        joins = [],
+        results,
+        config = {};
+
+	var Orchestrate = function(config) {
+		this.config = config;
+        this.wheres = [];
+        this.joins = [];
 	};
 
-	Orchestrate.prototype.select = function(string) {
-		// body...
-    	this.select_statement = string;
-	};
 
-    Orchestrate.prototype.from = function(endpoint) {
-    	this.endpoint = endpoint;
+    // check the config
+    // check endpoints exist
+    // set up each query
+    // process results
+    var buildQueries = function(global, callback) {
+        
+        config = global.config;
+
+        var configError = checkConfig(config)
+
+        if(configError !== false) {
+            callback(configError);
+        } else {
+            callback(null);
+        }
+
+        // build main query
+        // then loop joins
+
+        addQuery(global.getFrom(), global.getSelect(), global.getWhere(), global.getLimit(), global.getOrderBy().sortOn, global.getOrderBy().sortDir);
+        
+        var j = global.getJoins();
+        for (var i = j.length - 1; i >= 0; i--) {
+            addQuery(j[i].key, null, null, null, null, null, j[i].populateAs);
+        };
+
+        console.log(queries);
+    }
+
+    var addQuery = function(key, select, where, limit, sortOn, sortDir, populateAs) {
+
+        // query object
+        var obj = {
+            key: key
+        };
+
+        if(select) {
+            obj.select = select;
+        }
+
+        if(where) {
+            obj.where = where;
+        }
+
+        if(limit) {
+            obj.limit = limit;
+        }
+
+        if(sortOn) {
+            obj.sortOn = sortOn;
+        }
+
+        if(sortDir) {
+            obj.sortDir = sortDir;
+        }
+
+        // if join
+        if(populateAs) {
+            obj.populateAs = populateAs;
+        }
+
+        queries.push(obj);
+
     };
 
-    Orchestrate.prototype.where = function(property, operand, value) {
-        //
+    var checkConfig = function(conf) {
+
+        var error = false;
+
+        if(typeof conf === 'undefined') {
+            // does it exist
+            error = 'config must be passed to constructor';
+        } else if(typeof conf !== "object") {
+            // is it an object?
+            error = 'config must be an object';
+        }
+
+        return error;
+
+    }
+
+    // change this to node err
+    var checkEndpoints = function(endpoints) {
+
+        endpoints.forEach(function(endpoint){
+            
+            if(typeof endpoint.key !== 'string') {
+                throw new Error('Endpoint must have a string: key');
+            }
+
+            if(typeof endpoint.url !== 'string') {
+                throw new Error('Endpoint must have a string: url');
+            }
+
+            if(typeof endpoint.headers !== 'undefined' && typeof endpoint.headers !== 'object') {
+                throw new Error('headers must be supplied as an array');
+            }
+
+            if(typeof endpoint.map !== 'undefined' && typeof endpoint.map !== 'object') {
+                throw new Error('map must be supplied as an object');
+            }
+
+        });
+
     };
+
+    var checkEndpointExists = function(endpoints, key) {
+        
+        var find = _.findWhere(endpoints, {key: key});
+
+        if(typeof find === 'undefined') {
+            throw new Error('Key mismatch: ' + key);
+        }
+
+    };
+
+    // -------- Public API --------
+
+	Orchestrate.prototype.setSelect = function() {
+        
+        this.select_statement = _.flatten(arguments);
+        return this;
+
+    };
+
+    Orchestrate.prototype.getSelect = function() {
+        return this.select_statement;
+    };
+
+    Orchestrate.prototype.setFrom = function(endpoint) {
     
-    Orchestrate.prototype.join = function(entity, from_key, to_key) {
-    	//
+    	this.from_endpoint = endpoint;
+        return this;
+    
     };
 
-    Orchestrate.prototype.orderBy = function(property, dircetion) {
-    	//
+    Orchestrate.prototype.getFrom = function() {
+        return this.from_endpoint;    
     };
 
-    Orchestrate.prototype.limit = function(limit) {
-    	//
+    Orchestrate.prototype.setWhere = function(property, value) {
+        
+        this.wheres.push({
+            property: property,
+            value: value
+        });
+
+        return this;
+
+    };
+
+    Orchestrate.prototype.getWhere = function() {
+        return this.wheres;
+    };    
+
+    Orchestrate.prototype.setJoin = function(key, fromProperty, toProperty, populateAs) {
+    	
+        this.joins.push({
+            key: key,
+            fromProperty: fromProperty,
+            toProperty: toProperty,
+            populateAs: (typeof populateAs !== 'undefined' ? populateAs : key)
+        });
+
+        return this;
+
+    };
+
+    Orchestrate.prototype.getJoins = function() {
+        return this.joins;
+    };
+
+    Orchestrate.prototype.setOrderBy = function(property, direction) {
+    	this.sortOn = property;
+        this.sortDir = direction;
+    };
+
+    Orchestrate.prototype.getOrderBy = function() {
+        return {
+            sortOn: this.sortOn,
+            sortDir: this.sortDir
+        }
+    };
+
+    Orchestrate.prototype.setLimit = function(limit) {
+    	this.limit = limit;
+    };
+
+    Orchestrate.prototype.getLimit = function() {
+        return this.limit;
     };
 
     Orchestrate.prototype.get = function(callback) {
-    	// do stuff
-    	if(typeof callback === "function") {
-    		callback(); // pass data
-    	}
+
+        buildQueries(this, function(err, results) {
+        	
+            if(typeof callback === 'function') {
+        		callback(err, results); // pass data
+        	}
+
+        });
+
     };
 
     Orchestrate.prototype.count = function(callback) {
+
         // do stuff
         if(typeof callback === "function") {
             callback(); // pass data
         }
+
     };
 
     module.exports =  Orchestrate;
--- tests/test.js
+++ tests/test.js
@@ -1,52 +1,108 @@
-// var request = require('superagent');
 var expect = require('expect.js'),
-	Orchestrate = require('../orchestrate.js'),
+	Query = require('../orchestrate'),
+	config,
 	q;
 
 describe('Orchestrate', function(){
 
+	config = {
+		endpoints: [{
+			key: 'customers',
+			url: '/api/customers',
+			headers: [],
+			map: {sort: 'sort', direction: 'direction', limit: 'limit', offset: 'offset'}
+		},
+		{
+			key: 'bookings',
+			url: '/api/bookings',
+			headers: [],
+			map: {sort: 'sort', direction: 'direction', limit: 'limit', offset: 'offset'}
+		}]
+	}
+
 	beforeEach(function(){
-		q = new Orchestrate({foo:"bar"});
+		q = new Query(config);
 	});
 
 	it('should have set select statement', function(done){
   	
-		q.select('*');
+		q.setSelect('firstname');
 
-	  	expect(q.select_statement).to.equal('*');
+	  	expect(q.getSelect()[0]).to.equal('firstname');
 	  	done();
 	});
 
 	it('should have set from endpoint', function(done){
   	
-		q.from('customers');
+		q.setFrom('customers');
 
-	  	expect(q.endpoint).to.equal('customers');
+	  	expect(q.getFrom()).to.equal('customers');
 	  	done();
 	});
 
 	it('should have set select AND from endpoint', function(done){
   	
-		q.select('first_name');
-		q.from('customers');
+		q.setSelect('first_name');
+		q.setFrom('customers');
 
-	  	expect(q.select_statement).to.equal('first_name');
-	  	expect(q.endpoint).to.equal('customers');
+	  	expect(q.getSelect()[0]).to.equal('first_name');
+	  	expect(q.getFrom()).to.equal('customers');
 	  	done();
 	});
 
 	it('should set multiple instances', function(done){
 		
-		var customers = new Orchestrate();
-		var bookings = new Orchestrate();
+		var customers = new Query(config);
+		var bookings = new Query(config);
 		
-		customers.select('firstname');
-		bookings.select('start_time');
+		customers.setSelect('firstname');
+		bookings.setSelect('start_time');
 
-		expect(customers.select_statement).to.equal('firstname');
-		expect(bookings.select_statement).to.equal('start_time');
+		expect(customers.getSelect()[0]).to.equal('firstname');
+		expect(bookings.getSelect()[0]).to.equal('start_time');
 
 		done();
+	});
+
+	it('should set order by', function(done){
+		
+		q.setOrderBy('lastname', 'asc');
+		expect(q.getOrderBy().sortOn).to.equal('lastname');
+		expect(q.getOrderBy().sortDir).to.equal('asc');
+		done();
+	});
+
+	it('should set limit', function(done){
+		
+		q.setLimit(10);
+		expect(q.getLimit()).to.equal(10);
+		done();
+	});
+
+	it('should build join collection', function(done){
+		
+		q.setSelect('*').setFrom('customers').setJoin('bookings', '_id', 'customer_id');
+		expect(q.getFrom()).to.equal('customers');
+		expect(q.getJoins().length).to.equal(1);
+		expect(q.getJoins()[0].key).to.equal('bookings');
+		expect(q.getJoins()[0].fromProperty).to.equal('_id');
+		expect(q.getJoins()[0].toProperty).to.equal('customer_id');
+
+		done();
+	});
+
+	it('should return error as null', function(done){
+		
+		q.setSelect('firstname').setFrom('customers').setJoin('bookings', 'bookings.customer_id', 'customers._id', 'bookings').setWhere('customers._id', 123);
+
+		q.setLimit(100);
+		q.setOrderBy('lastname', 'desc');
+
+		q.get(function(err, data){
+			expect(err).to.equal(null);
+			done();
+		});
+		
 	});
 
 });
